e8d055836fa4108a09cd7c39983d4ae3d58ed620
Han Lazarus <han.lazarus@gmail.com>
2017-07-18 16:01:08 -0700

    twitter bot working

--- twitter_bot.py
+++ twitter_bot.py
@@ -1,120 +1,130 @@
-import tweepy
 import re
 import requests
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from app.models import SongLyrics
+import tweepy
 
-
-#
-
-# generate new song
-
-# check time
-# get last song
-# turn it into tweet
-# get url of last song
-# 
-
-# sleep
-
-
-# -- External connection approach
-# tablename is 'lyrics'
-# column name is 'song_lyrics'
-result = connection.execute("select song_lyrics from lyrics order by id desc limit 1")
-lyrics = result.fetchone()[0][:200]
-
-# ------------- Part of app approach
 from time import sleep
 import os
 
-def check_time_interval():
-    now = time.time()
-    now_str = int(now)
-
-    last_tweet_time_str = os.getenv('LAST_TWEET_TIME', now_str)
-    last_tweet_time = float(last_tweet_time_str)
-    
-    if now - last_tweet_time > tweet_interval:
-        os.putenv('LAST_TWEET_TIME', now_str)
-        return True
-
-    return False
-
-def get_song_query():
-    song_id = os.getenv('SONG_ID', False)
-    if song_id:
-      sql_query = "select song_lyrics from lyrics where id = " + song_id
-      return sql_query
-
-def get_
+URL = 'https://rkelly.hanhanhan.org/song-lyrics'
+TWEET_LENGTH = 140
+TWITTER_LINK_LENGTH = 23 + 1
 
 
-def post_to_twitter(f):
+def make_config_dict(f):
+  
+  d = {}
+
+  with open(f) as info:
+    for line in info:
+      var = line.strip().split('=')
+      if len(var) == 2:
+        d[var[0]] = var[1]
+
+    return d
 
 
-    # Note: On some platforms, including FreeBSD and Mac OS X, 
-    # setting environ may cause memory leaks. 
-    # Refer to the system documentation for putenv.
+def get_db_uri():
+  postgres_config = make_config_dict('rkelly.env')
+  database = postgres_config['DATABASE']
+  user = postgres_config['DATABASE_USER']
+  password = postgres_config['DATABASE_PASSWORD']
+
+  database_uri = 'postgresql://{}:{}@localhost/{}'.format(database, password, user) 
+
+  # database_uri ='postgresql://rkelly:hello@localhost/rkelly'
+  return database_uri 
 
 
-    SQLALCHEMY_DATABASE_URI ='postgresql://rkelly:hello@localhost/rkelly'
-    engine = create_engine(SQLALCHEMY_DATABASE_URI)
-    connection = engine.connect()
-      
-    result = connection.execute(sql_query)
-    lyrics = result.fetchone()[0][:200]
+def authenticate_twitter():
+  twitter_config = make_config_dict('twitter.env')
+
+  consumer_key = twitter_config['consumer_key']
+  consumer_secret = twitter_config['consumer_secret']
+  key = twitter_config['access_token']
+  secret = twitter_config['access_token_secret']
+
+  auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+  auth.set_access_token(key, secret)
+
+  api = tweepy.API(auth)
+  return api
 
 
-    time.sleep(tweet_interval)
+def generate_song():
+  # generate new robot kelly song, and return its url
 
-    
-
-
-
-
-# --------- Twitter
-
-# to move
-# twitter_config = {
-#   'consumer_key':         
-#   'consumer_secret':      
-#   'access_token':         
-#   'access_token_secret':  
-#   'timeout_ms':           
-# }
-
-consumer_key = twitter_config['consumer_key']
-consumer_secret = twitter_config['consumer_secret']
-key = twitter_config['access_token']
-secret = twitter_config['access_token_secret']
-
-# do i need a callback url?
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(key, secret)
-
-api = tweepy.API(auth)
-api = tweepy.API(auth)
+  r = requests.get(URL)
+  song_url = r.url
+  return song_url
 
 
-def tweet(song):  
-    # line = song
-    line = "I got you got ya jumpin \n Hits lots of chips and trips on you and your time\nPeace our soldiers home\nJust keepin it real i'll be her #1 man\nWe We'll be making sweet love night till day\nAnd all these things for you"
-    link = "http://rkelly.hanhanhan.org/"
-    chars = 140 - 24
-    line = line[:chars]
-    tweet = line + " " + link
-    tweet = tweet[:]
+def get_lyrics(song_id, database_uri):
+  if song_id is None:
+    song_id=5
+
+  engine = create_engine(database_uri)
+  connection = engine.connect()
+
+  sql_query = "SELECT * FROM lyrics WHERE id = {}".format(song_id)
+  result = connection.execute(sql_query)
+  lyrics = result.first()[1]
+
+  # necessary? would this close on exiting the function?
+  connection.close()
+
+  return lyrics
+
+
+def twitterify_lyrics(lyrics):
+  formatted_lyrics = re.sub('<br>(<br>)?','\n',lyrics).strip()
+  lyrics_length = TWEET_LENGTH - TWITTER_LINK_LENGTH - 4
+  lines = formatted_lyrics.split('\n')
+
+  short_lyrics = ''
+  running_length = 0
+
+  for line in lines:
+    running_length += len(line) + 1
+    if running_length >= lyrics_length:
+      break
+    else:    
+      short_lyrics = short_lyrics + '\n' + line
+
+  if short_lyrics == '':
+    short_lyrics = formatted_lyrics[:lyrics_length]
+
+  return short_lyrics
+
+
+def tweet(short_lyrics, api, url):  
+
+    tweet = short_lyrics + "...\n" + url
+
     print(tweet)
-    # try:
-    #     api.update_status(tweet)
-    # except tweepy.TweepError as e:
-    #     print('This happened :( {}'.format((e.api_code)))
+    try:
+        api.update_status(tweet)
+    except tweepy.TweepError as e:
+        print('This happened :( {}'.format((e.api_code)))
 
+def main():
+  api = authenticate_twitter()
+  song_url = generate_song()
+  regex = '(?:{})(\d+)'.format(URL)
+  song_id = re.search(regex, song_url)
+
+  db_uri = get_db_uri()
+  lyrics = get_lyrics(song_id, db_uri)
+
+  short_lyrics = twitterify_lyrics(lyrics)
+  tweet(short_lyrics, api, song_url)
 
 
 
 
 # 23 characters in t.co link
+
+if __name__ == '__main__':
+  main()
